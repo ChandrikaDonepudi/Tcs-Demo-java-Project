17cf13bd4d1c3a59a6e1de6248b33f31b410bd5f
chandrika <chandrika9720@gmail.com>
2022-03-21 22:29:48 -0700

    First Commit inside java project

---
+++ Demo-Tcs-Java-project/src/com/tcs/demo/Tcsdemo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.tcs.demo;
+
+/**
+ * @author springuser08
+ *
+ */
+public class Tcsdemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tcs demo project");
+
+	}
+
+}
---
+++ Demo-Tcs-Java-project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author springuser08
+ *
+ */
+module module {
+}
